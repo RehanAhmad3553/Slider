69ac9a76291f37658a6f0a18874a71f009c269f1
RehanAhmad3553 <117620259+RehanAhmad3553@users.noreply.github.com>
2022-12-26 21:37:01 +0500

    First Version

---
+++ app/src/main/java/com/example/testclass/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.testclass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.testclass.databinding.ActivityMainBinding;
+
+import org.imaginativeworld.whynotimagecarousel.model.CarouselItem;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+
+
+   binding.carousel.addData(new CarouselItem("https://www.shutterstock.com/image-vector/discount-banner-shape-special-offer-260nw-1467549638.jpg","Your text here"));
+   binding.carousel.addData(new CarouselItem("https://img.freepik.com/free-vector/mega-sale-offers-banner-template_1017-31299.jpg",""));
+   binding.carousel.addData(new CarouselItem("https://www.shutterstock.com/image-vector/red-banner-special-offer-260nw-1035467014.jpg",""));
+   binding.carousel.addData(new CarouselItem("https://www.shutterstock.com/image-vector/red-banner-special-offer-260nw-1035467014.jpg",""));
+
+
+
+
+
+
+       
+
+    }
+}
